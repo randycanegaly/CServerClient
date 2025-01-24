e6c67c62af254a2fb5a35c2ad7fd40720528378f
randycanegaly <canegaly@gmail.com>
2025-01-23 21:22:15 -0500

    trying to resolve workflow failure

--- server.c
+++ server.c
@@ -35,7 +35,7 @@ gethostname() — Who am I?
  *
  * REFACTORING - started on 1/21/25
  *  a) make server stay live and receive/handle multiple client connections for simple messages - DONE
- *  b) working on creating a GitHub workflow that will run a test upon push to a branch - IN PROGRESS on 1/23/25
+ *  b) working on creating a GitHub workflow that will run a test upon push to a branch - IN PROGRESS on 1/23/25. Aaaaand, it's not working. :(
  */
 
 // get sockaddr, IPv4 or IPv6:
